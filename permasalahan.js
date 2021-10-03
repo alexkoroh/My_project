bc6ea2d1083789276394642d0890d363fe2b1136
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-10-03 20:24:57 +0800

    Add files via upload

---
+++ permasalahan.js
@@ -0,0 +1,18 @@
+function beratBadanPria (tinggi){
+    let ideal = (tinggi - 100) - (tinggi -100) * 0.1
+return `${ideal} Kg`;    
+}
+
+//! Fungsi menghitung berat badan idela wanita
+function beratBadanWanita(tinggi){
+    let ideal = (tinggi - 100) - (tinggi -100) * 0.15
+    return `${ideal} Kg`;    
+}
+
+module.exports = {beratBadanPria, beratBadanWanita}
+
+
+//! Buat ini dua file di dalam datu folder...truss nnti non jalankankan yang app.js.....OK
+
+
+
