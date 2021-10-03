0cbe6b1cccd7f6edabf7a732e4acebf4d8dfa12f
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-10-04 04:59:47 +0800

    Alex project

---
+++ permasalahan.js
@@ -0,0 +1,41 @@
+function beratBadanPria (tinggi){
+    let ideal = (tinggi - 100) - (tinggi -100) * 0.1   
+     return `Pria =  ${ideal} Kg`;    
+
+}
+
+//! Fungsi menghitung berat badan ideal wanita
+function beratBadanWanita(tinggi){
+    let ideal = (tinggi - 100) - (tinggi -100) * 0.15
+    return `Wanita = ${ideal} Kg`;    
+}
+
+
+
+//!Fungsi pemilihan.....
+function pemilihan(jenis,tinggi,nama){
+    var pilih ;
+    if(jenis == 1){
+        pilih = beratBadanPria(tinggi)
+        return pilih;
+    }else if(jenis == 2){
+        var pilih2;
+        pilih2 = beratBadanWanita(tinggi)
+            return pilih2;
+        
+    }else if(jenis == 3){
+        return(`Eror...Lu Lgbt Anjir`);
+    }
+    else{
+        return `Erorr`;
+    }
+}
+
+
+module.exports = {beratBadanPria, beratBadanWanita, pemilihan}
+
+
+//! Buat ini dua file di dalam datu folder...truss nnti non jalankankan yang app.js.....OK
+
+
+
