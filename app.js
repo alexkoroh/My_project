5a565037610c66305796ccba067139418cc30bf4
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-10-09 11:52:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ var server = http.createServer(function(request,response){
         
     
     `</table>` + '<br><br>' +
-                        'Berat badan Ideal  ' + pemilihan(eval(form['jenis']),eval(form['tinggi']),eval(form['nama']))
+                        'Berat badan Ideal  ' + ideal.pemilihan(eval(form['Jenis']),eval(form['Tinggi']),eval(form['nama']))
                     
                 )
             })
